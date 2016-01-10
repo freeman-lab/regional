8cbc6244c7db81dda301f4eec0b16ccfdbc9c751
freeman <the.freeman.lab@gmail.com>
2016-01-10 14:12:06 -0500

    Bootstrap layout

---
+++ setup.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python
+
+from setuptools import setup
+import regional
+
+version = '1.0.0'
+
+setup(
+    name='regional',
+    version=version,
+    description='simple manipualtion and display of spatial regions',
+    author='freeman-lab',
+    author_email='the.freeman.lab@gmail.com',
+    packages=['regional'],
+    url='https://github.com/freeman-lab/regional',
+    install_requires=open('requirements.txt').read().split(),
+    long_description='See https://github.com/freeman-lab/regional'
+)
